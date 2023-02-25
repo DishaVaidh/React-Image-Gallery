20443e8892364b21d209325b6db19d963b7c1b0e
Disha Vaidh <dishavaidh123@gmail.com>
2023-02-26 01:11:21 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/Navbar";
+import Image from "./components/Image";
+class App extends React.Component {
+  state = {
+    category: "random",
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleCallback = (childData) => {
+    this.setState({ category: childData });
+  };
+
+  render() {
+    const { category } = this.state;
+    return (
+      <div>
+        <NavBar parentCallback={this.handleCallback} />
+        <Image category={category} />
+      </div>
+    );
+  }
 }
-
 export default App;
